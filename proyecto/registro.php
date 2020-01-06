307c0921c6aa624f80b8aec09429d0b7756ff038
Luis Fernández López <55986399+darkkail@users.noreply.github.com>
2020-01-06 18:26:50 -0300

    Add files via upload

---
+++ proyecto/registro.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content ="width=device-width, inial-scale=1">
+    <title>REGISTRO DE SOCIOS</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <div class="form-group">
+        <label class="control-label col-xs-3">Rut:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control"  placeholder="Rut">
+        </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Email:</label>
+        <div class="col-xs-9">
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Password:</label>
+        <div class="col-xs-9">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Confirmar Password:</label>
+        <div class="col-xs-9">
+            <input type="password" class="form-control" placeholder="Confirmar Password">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Nombre:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control" placeholder="Nombre">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Apellido:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control" placeholder="Apellido">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3" >Telefono:</label>
+        <div class="col-xs-9">
+            <input type="tel" class="form-control" placeholder="Telefono">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">F. Nacimiento:</label>
+        <div class="col-xs-3">
+            <select class="form-control">
+                <option>Dia</option>
+            </select>
+        </div>
+        <div class="col-xs-3">
+            <select class="form-control">
+                <option>Mes</option>
+            </select>
+        </div>
+        <div class="col-xs-3">
+            <select class="form-control">
+                <option>Año</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Dirección:</label>
+        <div class="col-xs-9">
+            <textarea rows="3" class="form-control" placeholder="Dirección"></textarea>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Genero:</label>
+        <div class="col-xs-2">
+            <label class="radio-inline">
+                <input type="radio" name="genderRadios" value="male"> Maculino
+            </label>
+        </div>
+        <div class="col-xs-2">
+            <label class="radio-inline">
+                <input type="radio" name="genderRadios" value="female"> Femenino
+            </label>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <label class="checkbox-inline">
+                <input type="checkbox" value="news"> Enviar noticias.
+            </label>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <label class="checkbox-inline">
+                <input type="checkbox" value="agree">  Accepto <a href="#">Terminos y condiciones</a>.
+            </label>
+        </div>
+    </div>
+    <br>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <input type="submit" class="btn btn-primary" value="Enviar">
+            <input type="reset" class="btn btn-default" value="Limpiar">
+        </div>
+    </div>
+</form>
